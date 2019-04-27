1e7414d110e82b869130bc6af1ffa4859d251c4a
namirali <n.amirali@gmail.com>
2019-04-27 12:57:32 +0300

    html2pdf tests

---
+++ html2pdf_test.go
@@ -0,0 +1,59 @@
+package gorestpack
+
+import (
+	"os"
+	"testing"
+)
+
+func Test_HTML2PDF_ValidToken(t *testing.T) {
+	pdfToken := os.Getenv("PDF_TOKEN")
+
+	client := NewHTMLToPDFClient(pdfToken)
+	_, err := client.Capture("https://google.com/")
+
+	if err != nil {
+		t.Errorf("Error: %s", err.Error())
+	}
+}
+
+func Test_HTML2PDF_InvalidToken(t *testing.T) {
+	client := NewHTMLToPDFClient("INVALID_TOKEN")
+	_, err := client.Capture("https://google.com/")
+
+	if err == nil {
+		t.Errorf("Must return error with invalid token")
+	}
+}
+
+func Test_HTML2PDF_404(t *testing.T) {
+	pdfToken := os.Getenv("PDF_TOKEN")
+
+	client := NewHTMLToPDFClient(pdfToken)
+	_, err := client.Capture("https://google/")
+
+	if err == nil {
+		t.Errorf("Must return error with net::ERR_NAME_NOT_RESOLVED: %s", err.Error())
+	}
+}
+
+func Test_HTML2PDF_ValidToken_Render(t *testing.T) {
+	pdfToken := os.Getenv("PDF_TOKEN")
+
+	client := NewHTMLToPDFClient(pdfToken)
+	_, err := client.CaptureToReader("https://google.com")
+
+	if err != nil {
+		t.Errorf("Error: %s", err.Error())
+	}
+}
+
+func Test_HTML2PDF_ValidToken_Render_404(t *testing.T) {
+	pdfToken := os.Getenv("PDF_TOKEN")
+
+	client := NewHTMLToPDFClient(pdfToken)
+	_, err := client.CaptureToReader("https://google.coddm")
+
+	if err == nil {
+		t.Errorf("Must return error with net::ERR_NAME_NOT_RESOLVED: %s", err.Error())
+	}
+}
